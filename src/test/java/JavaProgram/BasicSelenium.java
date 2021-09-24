69622994b0526b3a51de39ee4bbb9bb8b1674dbf
jashwanthravuru <80572166+jashwanthravuru@users.noreply.github.com>
2021-09-24 10:28:36 +0530

    my third commit

---
+++ src/test/java/JavaProgram/BasicSelenium.java
@@ -0,0 +1,20 @@
+package JavaProgram;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+System.setProperty("webdriver.chrome.driver", "D:\\Sai\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://demowebshop.tricentis.com/");
+		driver.manage().window().maximize();
+
+	}
+
+}
