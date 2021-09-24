96fc6f956ccca5636a2e51a318f2740fb8fd6ee8
jashwanthravuru <80572166+jashwanthravuru@users.noreply.github.com>
2021-09-24 11:09:24 +0530

    my fifth commit

--- src/test/java/JavaProgram/JavaProgram.java
+++ src/test/java/JavaProgram/JavaProgram.java
@@ -5,10 +5,10 @@ public class JavaProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		 int num1 = 9, num2 = 15, sum;
-	      sum = num1 + num2;
+		// int num1 = 9, num2 = 15, sum;
+	    //  sum = num1 + num2;
 
-	      System.out.println("Sum of these numbers: "+sum);
+	     // System.out.println("Sum of these numbers: "+sum);
 	   }
 	
 
