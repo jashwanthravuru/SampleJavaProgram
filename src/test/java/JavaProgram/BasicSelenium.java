3846d6fbe5ab0b35d4a2d063d28545a3ea86b61b
jashwanthravuru <80572166+jashwanthravuru@users.noreply.github.com>
2021-09-25 22:33:56 +0530

    my fifth commit

--- src/test/java/JavaProgram/BasicSelenium.java
+++ src/test/java/JavaProgram/BasicSelenium.java
@@ -14,6 +14,7 @@ System.setProperty("webdriver.chrome.driver", "D:\\Sai\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("http://demowebshop.tricentis.com/");
 		driver.manage().window().maximize();
+		driver.close();
 
 	}
 
